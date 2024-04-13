d4e0b39ca944bcb405cdda203b277fa7f1f774af
TooniOlaniyan <asalamlatif@gmail.com>
2024-04-13 12:27:23 +0100

    header

--- components/shared/Header.jsx
+++ components/shared/Header.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { SearchProduct } from '.'
-
+import React from "react";
+import Image from "next/image";
+import { SearchProduct } from ".";
 
 const Header = () => {
   return (
-    <header className='w-full flex items-center justify-around'>
-        <div>BUYTHRIFT</div>
-        <SearchProduct/>
-        <div className='flex'>
-            <p>Account</p>
-            <p>Notification</p>
-            <p>Cart</p>
-        </div>
-
-
+    <header className="w-full flex items-center justify-between py-2 px-4">
+      <div>
+        <Image src='/logo.svg' alt="logo" width={150} height={150}/>
+      </div>
+      <SearchProduct />
+      <div className="flex items-center gap-8">
+        <Image className="cursor-pointer object-contain" src='/Account.svg' alt="cart" width={25} height={25}/>
+        <Image className="cursor-pointer object-contain" src='/Notification.svg' alt="cart" width={25} height={25}/>
+        <Image className="cursor-pointer object-contain" src='/bag.svg' alt="cart" width={25} height={25}/>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- components/shared/SearchProduct.jsx
+++ components/shared/SearchProduct.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Input } from "@/components/ui/input";
+import Image from 'next/image';
+
 
 const SearchProduct = () => {
   return (
-    <div>SearchProduct</div>
-  )
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input className='' type="text" placeholder="Search Products..." />
+      <Image src="/search.svg" alt="search" width={25} height={25} />
+    </div>
+  );
 }
 
 export default SearchProduct
